1b9e9a7a0545fb5448c00b39e9a033f0ffe2d9e1
bryan <bryan.guner@gmail.com>
2020-10-14 14:12:14 -0400

    prac problems

--- async-await-practice/your-code/problem_1.js
+++ async-await-practice/your-code/problem_1.js
@@ -6,8 +6,6 @@ function stretch() {
     }, 1000);
   });
 }
-
-
 function runOnTreadmill() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -16,8 +14,6 @@ function runOnTreadmill() {
     }, 500);
   });
 }
-
-
 function liftWeights() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -26,22 +22,42 @@ function liftWeights() {
     }, 2000);
   });
 }
-
-
 // TODO: use async & await to create a function called workout that runs the 
 // above functions in a way that ensures you begin runningOnTreadmill after 
 // you're finished stretching, you being liftWeights after you've finished 
 // running on the treadmill and console.logs "done working out" after 
 // you've finished lifting weights
-
-
-
-
-
-
 // TESTING: 
-// - run the file (node problem_1.js) to see if you get the expected output
+// async function workout() {
+//   try {
+//     await stretch();
+//     await runOnTreadmill();
+//     await liftWeights();
+//     console.log("done working out");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
+//Alternatively 
+async function workout() {
+    await stretch();
+    await runOnTreadmill();
+    await liftWeights();
+    console.log("done working out");
+   
+  
+}
+workout().catch(error=>{
+  console.error("error");
+});
+/*
+done stretching
+done running on treadmill
+done lifting weights
+done working out
+*/
+// - run the file (node problem_1.js) to see if you get the expected output
 workout();
 // done stretching
 // done running on treadmill
--- async-await-practice/your-code/problem_2.js
+++ async-await-practice/your-code/problem_2.js
@@ -11,32 +11,25 @@ function stretch(timeLeft) {
 		}
 	})
 }
-
-
 function runOnTreadmill(timeLeft) {
 	return new Promise((resolve, reject) => {
 		if (timeLeft < 500) {
 			reject('you dont have enough time to run on treadmill')
 		} else {
 			timeLeft -= 500;
-
 			setTimeout(() => {
 				console.log('done running on treadmill');
 				resolve(timeLeft)
 			}, 500)
 		}
-
 	})
 }
-
-
 function liftWeights(timeLeft) {
 	return new Promise((resolve, reject) => {
 		if (timeLeft < 2000) {
 			reject('you dont have enough time to lift weights')
 		} else {
 			timeLeft -= 2000
-
 			setTimeout(() => {
 				console.log('done lifting weights');
 				resolve(timeLeft)
@@ -44,12 +37,8 @@ function liftWeights(timeLeft) {
 		}
 	})
 }
-
-
 // TODO: reacreate the following solution to problem #2 from this morning's
 // promise-warmup practice using async & await 
-
-
 // function workout(totalTime) {
 //   stretch(totalTime)
 //     .then((timeLeftAfterStretching) => runOnTreadmill(timeLeftAfterStretching))
@@ -59,33 +48,36 @@ function liftWeights(timeLeft) {
 //     )
 //     .catch((err) => console.log("Error: ", err));
 // }
+async function workout(totalTime){
+	totalTime = await stretch(totalTime);
+	totalTime = await runOnTreadmill(totalTime);
+	totalTime = await liftWeights(totalTime);
+	console.log(`done working out with ${totalTime / 1000} seconds left`)
+}
 
 
+workout(4000).catch(err =>{
+	console.error(err)
+})
 
-
-
-
-
-
+/*
+done stretching
+done running on treadmill
+done lifting weights
+done working out with 0.5 seconds left
+*/
 // TESTING: 
 // - comment in each invocation of your workout function below and run 
 // the file (node problem_2.js) to see if you get the expected output
-
 // workout(500)
 //    Error:  you dont have enough time to stretch
-
-
 // workout(1000);
 // 		done stretching
 // 		Error:  you dont have enough time to run on treadmill
-
-
 // workout(2000);
 // 		done stretching
 // 		done running on treadmill
 // 		Error:  you dont have enough time to lift weights
-
-
 // workout(4000);
 // 		done stretching
 // 		done running on treadmill
