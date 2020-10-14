a3e83db7818d8dc1d01d00fce640e67b39dfd1f5
William Vincent <compelledtograce@gmail.com>
2020-10-14 19:14:37 -0400

    Refactored and fixed bugs

--- Trivia-game-three-ways/game.js
+++ Trivia-game-three-ways/game.js
@@ -1,20 +1,48 @@
 import { getClueFromCallback } from "./callbackVersion.js";
 
-function setInnerHTML(id, object) {
-  let element = document.getElementById(id);
-  element.innerHTML = object[id];
-}
-const cbBtn = document.getElementById("use-callback");
-cbBtn.addEventListener("click", (event) => {
-  getClueFromCallback((err, clueObj) => {
-    console.log(clueObj);
-    if (err !== null) {
-      console.error(err);
-    } else {
-      let idArr = ["question", "answer", "value", "catagoryTitle"];
-      idArr.forEach((ele) => {
-        setInnerHTML(ele, clueObj);
-      });
+document.addEventListener("DOMContentLoaded", () => {
+  const responseButton = document.getElementById("check-response");
+  const playerInput = document.getElementById("player-response");
+  const cbBtn = document.getElementById("use-callback");
+  const idArr = ["question", "answer", "value", "category-title"];
+  const ele = document.getElementById("invalid-count");
+
+  const setInnerHTML = (id, object) => {
+    if (object[id] !== null) {
+      if (id !== "catagory-title") {
+        let element = document.getElementById(id);
+        element.innerHTML = object[id];
+      }
+      if (id === "category-title") {
+        let element = document.getElementById(id);
+        element.innerHTML = object.category.title;
+      }
     }
+  };
+
+  const setInvalid = (object) => {
+    if (object.invalidCount > 0 && object.invalidCount !== undefined) {
+      ele.innerHTML = "invalid";
+    } else {
+      ele.innerHTML = "valid";
+    }
+  };
+
+  cbBtn.addEventListener("click", (event) => {
+    getClueFromCallback((err, clueObj) => {
+      console.log(clueObj);
+      if (err !== null) {
+        console.error(err);
+      } else {
+        idArr.forEach((ele) => {
+          setInnerHTML(ele, clueObj);
+        });
+        setInvalid(clueObj);
+      }
+      responseButton.addEventListener("click", (event) => {
+        if (playerInput === clueObj.answer) {
+        }
+      });
+    });
   });
 });
