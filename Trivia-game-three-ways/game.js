88b6428911ed278ee1e682d37c3a2751fc1b6f5b
bryan <bryan.guner@gmail.com>
2020-10-14 22:16:35 -0400

    with promises

--- Trivia-game-three-ways/game.js
+++ Trivia-game-three-ways/game.js
@@ -1,48 +1,55 @@
-import { getClueFromCallback } from "./callbackVersion.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const responseButton = document.getElementById("check-response");
-  const playerInput = document.getElementById("player-response");
-  const cbBtn = document.getElementById("use-callback");
-  const idArr = [
-    "question",
-    "answer",
-    "value",
-    "category-title",
-    "invalidCount",
-  ];
-  const checkValid = document.getElementById("invalid-count");
-
-  const setInnerHTML = (id, object) => {
-    let element = document.getElementById(id);
-
-    if (object[id] !== null) {
-      if (id === "invalidCount") {
-        object.invalidCount > 0 && object.invalidCount !== undefined
-          ? (checkValid.innerHTML = "invalid")
-          : (checkValid.innerHTML = "valid");
-      } else if (id === "category-title") {
-        element.innerHTML = object.category.title;
-      } else {
-        element.innerHTML = object[id];
-      }
+import {
+  getClueFromCallback
+} from "./callbackVersion.js";
+import {
+  getClue as getClueFromPromise
+} from "./promise-version.js";
+document.addEventListener( "DOMContentLoaded", () => {
+const responseButton = document.getElementById( "check-response" );
+const playerInput = document.getElementById( "player-response" );
+const cbBtn = document.getElementById( "use-callback" );
+const promBtn = document.getElementById( 'use-promise' );
+const idArr = [
+  "question",
+  "answer",
+  "value",
+  "category-title",
+  "invalidCount",
+];
+const checkValid = document.getElementById( "invalid-count" );
+const setInnerHTML = ( id, object ) => {
+  let element = document.getElementById( id );
+  if ( object[ id ] !== null ) {
+    if ( id === "invalidCount" ) {
+      object.invalidCount > 0 && object.invalidCount !== undefined ?
+        ( checkValid.innerHTML = "invalid" ) :
+        ( checkValid.innerHTML = "valid" );
+    } else if ( id === "category-title" ) {
+      element.innerHTML = object.category.title;
+    } else {
+      element.innerHTML = object[ id ];
     }
-  };
+  }
+};
+//--------------------------with promise--------------------------------------
+promBtn.addEventListener( "click", ( event ) => {
+  const get = getClueFromPromise()
+  console.log( get );
+  idArr.forEach( ( ele ) => {
+    setInnerHTML( ele, get );
+  } );
+} );
+//--------------------------with callback--------------------------------------
+cbBtn.addEventListener( "click", ( event ) => {
+  getClueFromCallback( ( err, clueObj ) => {
+    console.log( clueObj );
+    err !== null ?
+      console.error( err ) :
+      idArr.forEach( ( ele ) => {
+        setInnerHTML( ele, clueObj );
+      } );
+  } );
+} );
+} );
+//--------------------
 
-  cbBtn.addEventListener("click", (event) => {
-    getClueFromCallback((err, clueObj) => {
-      console.log(clueObj);
-      err !== null
-        ? console.error(err)
-        : idArr.forEach((ele) => {
-            setInnerHTML(ele, clueObj);
-          });
-    });
-  });
-  // Apparently this is implemented at the end. After the promise and async/await methods are built.
-  //   responseButton.addEventListener("click", (event) => {
-  //     if (playerInput.value === clueObj.answer) {
-  //     }
-  //   });
-  // });
-});
